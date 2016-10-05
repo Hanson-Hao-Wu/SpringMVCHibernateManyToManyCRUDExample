75ad36db1047925c5b161b93200a38d2250e14e6
Hanson-Hao-Wu <hanson.hao.wu@gmail.com>
2016-10-05 16:48:17 -0400

    first commit

---
+++ src/main/java/org/hao/springmvc/model/UserProfileType.java
@@ -0,0 +1,22 @@
+package org.hao.springmvc.model;
+
+public enum UserProfileType {
+	
+	USER("USER"),
+    DBA("DBA"),
+    ADMIN("ADMIN");
+     
+    String userProfileType;
+    
+    private UserProfileType(String userProfileType){
+        this.userProfileType = userProfileType;
+    }
+
+	public String getUserProfileType() {
+		return userProfileType;
+	}
+
+	public void setUserProfileType(String userProfileType) {
+		this.userProfileType = userProfileType;
+	}
+}
